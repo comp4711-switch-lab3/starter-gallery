6db5848dccccdac6c899ff699b09952669251377
jade303 <tomjmckay@hotmail.ca>
2018-01-28 16:41:27 -0800

    Implemented new template in codeigniter

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,13 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends Application
 {
-
-	/**
-	 * Index Page for this controller.
-	 */
 	public function index()
 	{
-      $this->load->view('welcome');
+//      $this->load->view('welcome');
+		$this->data['pagebody'] = 'welcome';
+		$this->render();
 	}
 
 }
