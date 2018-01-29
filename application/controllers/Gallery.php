79ed50f8908d5d7feab7d6a5661a65731dc6d066
philip <pwr_two@hotmail.com>
2018-01-28 18:45:16 -0800

    commit

--- application/controllers/Gallery.php
+++ application/controllers/Gallery.php
@@ -9,25 +9,24 @@ class Gallery extends Application
    */
 	public function index()
 	{
-      // //$this->load->view('gallery');
-      // // get all the images from our model
-      // $pix = $this->images->all();
+      // get all the images from our model
+      $pix = $this->images->all();
       
-      // // build an array of formatted cells for them
-      // foreach($pix as $picture)
-          // $cells[] = $this->parser->parse('_cell', (array) $picture, true);
+      // build an array of formatted cells for them
+      foreach($pix as $picture)
+          $cells[] = $this->parser->parse('_cell', (array) $picture, true);
           
-      // $this->load->library('table');
-      // $parms = array(
-          // 'table_open' => '<table class="gallery">',
-          // 'cell_start' => '<td class="oneimage">',
-          // 'cell_alt_start' => '<td class="oneimage">'
-          // );
-      // $this->table->set_template($parms);
+      $this->load->library('table');
+      $parms = array(
+          'table_open' => '<table class="gallery">',
+          'cell_start' => '<td class="oneimage">',
+          'cell_alt_start' => '<td class="oneimage">'
+          );
+      $this->table->set_template($parms);
       
-      // // finally generate table
-      // $rows = $this->table->make_columns($cells, 3);
-      // $this->data['thetable'] = $this->table->generate($rows);
+      // finally generate table
+      $rows = $this->table->make_columns($cells, 3);
+      $this->data['thetable'] = $this->table->generate($rows);
       
       $this->data['pagebody'] = 'gallery';
       $this->render();
